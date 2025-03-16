6e07e58e345d6a366a358dfbb6db92eb94d32040
Madenov Begis <begismadenov@gmail.com>
2025-03-16 15:56:02 +0500

    chabge base url

--- src/shared/constants/base-url.ts
+++ src/shared/constants/base-url.ts
@@ -1,2 +1,2 @@
-// export const BASE_URL = 'https://deliver-shop.uz/api/admin/v1'
-export const BASE_URL = 'https://botshop.jprq.site/api/admin/v1'
+export const BASE_URL = 'https://api.deliver-shop.uz/api/admin/v1'
+// export const BASE_URL = 'https://botshop.jprq.site/api/admin/v1'
